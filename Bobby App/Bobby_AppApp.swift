385c3e0f3e222d6af762ff85a5d5cf7bef983f8d
Meagan <meagansimyh@gmail.com>
2025-08-02 09:59:33 +0800

    Initial Commit

---
+++ Bobby App/Bobby_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bobby_AppApp.swift
+//  Bobby App
+//
+//  Created by Meagan Sim on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Bobby_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bobby App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bobby App
+//
+//  Created by Meagan Sim on 2/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
