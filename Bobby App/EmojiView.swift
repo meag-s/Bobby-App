1784e6ca49934a30660a3f76191cd5d619c349d6
Meagan <meagansimyh@gmail.com>
2025-08-02 10:40:48 +0800

    EmojiView

---
+++ Bobby App/EmojiView.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiView.swift
+//  Bobby App
+//
+//  Created by Meagan Sim on 2/8/25.
+//
+
+import SwiftUI
+
+struct EmojiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmojiView()
+}
